d21cfc2aa074f6fd2d65427457061bf7e3afe941
Jirlan Souza <jirlansouza08@gmail.com>
2025-07-06 10:05:57 -0400

    feat(diagram): Add Connection class
       Introduces the `Connection` class to represent
       connections between component instances within a diagram.
       Supports both port and signal connections.

---
+++ src/domain/diagram/model/connection.cpp
@@ -0,0 +1,61 @@
+/*
+ * plant-composer
+ * Copyright (c) 2025 jirlansouza
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.nu/licenses/>.
+ */
+
+#include "connection.h"
+
+namespace domain::diagram {
+    Connection::Connection(
+        const std::string &id,
+        const Type type,
+        const std::string &sourceComponentId,
+        const std::string &sourceIoId,
+        const std::string &targetComponentId,
+        const std::string &targetIoId
+    )
+        : id_(id),
+        type_(type),
+        sourceComponentId_(sourceComponentId),
+        sourceIoId_(sourceIoId),
+        targetComponentId_(targetComponentId),
+        targetIoId_(targetIoId) {
+    }
+
+    std::string Connection::getId() const {
+        return id_;
+    }
+
+    Connection::Type Connection::getType() const {
+        return type_;
+    }
+
+    std::string Connection::getSourceComponentId() const {
+        return sourceComponentId_;
+    }
+
+    std::string Connection::getSourceIoId() const {
+        return sourceIoId_;
+    }
+
+    std::string Connection::getTargetComponentId() const {
+        return targetComponentId_;
+    }
+
+    std::string Connection::getTargetIoId() const {
+        return targetIoId_;
+    }
+}
---
+++ src/domain/diagram/model/connection.h
@@ -0,0 +1,59 @@
+/*
+ * plant-composer
+ * Copyright (c) 2025 jirlansouza
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <string>
+
+namespace domain::diagram {
+    class Connection {
+    public:
+        enum class Type {
+            Port, Signal
+        };
+
+        Connection(
+            const std::string &id,
+            Type type,
+            const std::string &sourceComponentId,
+            const std::string &sourceIoId,
+            const std::string &targetComponentId,
+            const std::string &targetIoId
+        );
+
+        [[nodiscard]] std::string getId() const;
+
+        [[nodiscard]] Type getType() const;
+
+        [[nodiscard]] std::string getSourceComponentId() const;
+
+        [[nodiscard]] std::string getSourceIoId() const;
+
+        [[nodiscard]] std::string getTargetComponentId() const;
+
+        [[nodiscard]] std::string getTargetIoId() const;
+
+    private:
+        std::string id_;
+        Type type_;
+        std::string sourceComponentId_;
+        std::string sourceIoId_;
+        std::string targetComponentId_;
+        std::string targetIoId_;
+    };
+}
